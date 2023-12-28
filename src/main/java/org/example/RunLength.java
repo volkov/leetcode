b4b53c059e53b1c6a8597c68dd282edbb2a9b435
Sergei Volkov <serg-v@toloka.ai>
2023-12-28 15:39:55 +0100

    correct solution for run length

--- src/main/java/org/example/RunLength.java
+++ src/main/java/org/example/RunLength.java
@@ -1,129 +1,61 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class RunLength {
-    
-    record Item(char symbol, int count) {
 
-    }
-    
     public int getLengthOfOptimalCompression(String s, int k) {
-        List<Item> items = getItems(s);
-        System.out.println(items);
-
-        while (k > 0) {
-            int indexToDelete = 0;
-            int countToDelete = 0;
-            double maxDiff = 0;
-            for (int j = 1; j <= k; j++) {
-                for (int i = 0; i < items.size(); i++) {
-                    var diff = 0;
-                    var current  = items.get(i);
-
-                    if (current.count() - j == 0) {
-                        diff = weight(current.count);
-                        diff += collapseCount(items, i);
-                    }
-                    if (current.count() - j > 0) {
-                        diff = weight(current.count) - weight(current.count - j);;
-                    }
-                    if (diff * 1.0/j > maxDiff) {
-                        System.out.printf("New max diff %d on %d deleting %d%n", diff, i, j);
-                        maxDiff = diff * 1.0/j;
-                        indexToDelete = i;
-                        countToDelete = j;
-                    }
+        int[][] dp = new int[s.length() + 1][k + 1];
+        for (int i = 0; i <= s.length(); i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+        dp[0][0] = 0;
+        for (int i = 0; i <= s.length(); i++) {
+            for (int j = 0; j <= k; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
                 }
-            }
-            if (maxDiff > 0) {
-                delete(items, indexToDelete, countToDelete);
-                k-=countToDelete;
-            } else {
-                return calculate(items);
+                if (j > 0) {
+                    dp[i][j] = dp[i][j - 1];
+                }
+                if (i > 0 && j > 0) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1]);
+                }
+
+                int gropupSize = 1;
+                int toDelete = 0;
+                for (int l = i - 1; l >= 0; l--) {
+                    if (s.charAt(i - 1) != s.charAt(l)) {
+                        toDelete++;
+                    } else {
+                        gropupSize++;
+                    }
+                    if (toDelete > j) {
+                        break;
+                    }
+                    dp[i][j] = Math.min(dp[i][j], dp[l][j - toDelete] + groupToEncoding(gropupSize));
+                }
+
+                System.out.printf("dp[%d][%d] = %d%n", i, j, dp[i][j]);
             }
         }
-        return calculate(items);
+        return dp[s.length()][k];
     }
 
-    private int weight(int count) {
-        if (count == 0) {
-            return 0;
+    private int groupToEncoding(int i) {
+        if (i >= 100) {
+            return 4;
         }
-        if (count == 1) {
-            return 1;
-        }
-        if (count < 10) {
-            return 2;
-        }
-        if (count < 100) {
+        if (i >= 10) {
             return 3;
         }
-        return 4;
-    }
-
-    private int calculate(List<Item> items) {
-        System.out.println(items);
-        return items.stream().mapToInt(i -> weight(i.count)).sum();
-    }
-
-    private void delete(List<Item> items, int indexToDelete, int countToDelete) {
-        Item current = items.get(indexToDelete);
-        if (countToDelete < current.count) {
-            items.set(indexToDelete, new Item(current.symbol, current.count - countToDelete));
-        }
-        items.remove(indexToDelete);
-        if (indexToDelete == 0 || indexToDelete == items.size()) {
-            return;
-        }
-        if (items.get(indexToDelete - 1).symbol == items.get(indexToDelete).symbol) {
-            var toAdd = items.get(indexToDelete).symbol;
-            items.remove(indexToDelete);
-            var prev = items.get(indexToDelete - 1);
-            items.set(indexToDelete -1, new Item(prev.symbol, prev.count + toAdd));
-        }
-    }
-
-    private int collapseCount(List<Item> items, int i) {
-        if (i == 0 || i == items.size() - 1) {
-            return 0;
-        }
-        Item left = items.get(i - 1);
-        Item right = items.get(i + 1);
-        if (left.symbol() == right.symbol()) {
-            if (left.count() == 1 && right.count() == 1) {
-                return 0;
-            }
-            if (left.count() == 1 || right.count() == 1) {
-                return 1;
-            }
+        if (i > 1) {
             return 2;
         }
-        return 0;
-    }
-
-    private static List<Item> getItems(String s) {
-        List<Item> items = new ArrayList<>();
-        char prev = 0;
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char current = s.charAt(i);
-            if (current != prev) {
-                if (count > 0) {
-                    items.add(new Item(prev, count));
-                }
-                count = 1;
-                prev = current;
-            } else {
-                count++;
-            }
-        }
-        items.add(new Item(prev, count));
-        return items;
+        return 1;
     }
 
     public static void main(String[] args) {
-        System.out.println(new RunLength().getLengthOfOptimalCompression("aaaaaaaaaaa", 0));
+        System.out.println(new RunLength().getLengthOfOptimalCompression("aaabcccd", 2));
     }
 }
